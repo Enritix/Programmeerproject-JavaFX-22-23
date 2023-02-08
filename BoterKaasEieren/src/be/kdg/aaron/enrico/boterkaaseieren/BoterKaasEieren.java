708a4d504d5f95d45418dcf93ba13692832d3fa3
aaronvanhove <vanhoveaaron@gmail.com>
2023-02-08 15:33:20 +0100

    b

--- BoterKaasEieren/src/be/kdg/aaron/enrico/boterkaaseieren/BoterKaasEieren.java
+++ BoterKaasEieren/src/be/kdg/aaron/enrico/boterkaaseieren/BoterKaasEieren.java
@@ -1,4 +1,11 @@
 package be.kdg.aaron.enrico.boterkaaseieren;
 
-public class BoterKaasEieren {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BoterKaasEieren extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
