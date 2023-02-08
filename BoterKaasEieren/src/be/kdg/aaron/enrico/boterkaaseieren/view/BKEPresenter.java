f26a1450efab21ef44e44eba247e48ea5cb60fac
aaronvanhove <vanhoveaaron@gmail.com>
2023-02-08 15:59:34 +0100

    b

--- BoterKaasEieren/src/be/kdg/aaron/enrico/boterkaaseieren/view/BKEPresenter.java
+++ BoterKaasEieren/src/be/kdg/aaron/enrico/boterkaaseieren/view/BKEPresenter.java
@@ -1,4 +1,17 @@
 package be.kdg.aaron.enrico.boterkaaseieren.view;
 
+import be.kdg.aaron.enrico.boterkaaseieren.model.BKEModel;
+
 public class BKEPresenter {
+    private BKEModel model;
+    private BKEView view;
+
+    public BKEPresenter(BKEModel model, BKEView view) {
+        this.model = model;
+        this.view = view;
+        addEventHandlers();
+    }
+    private void addEventHandlers() {
+       //TODO button bullshit
+    }
 }
