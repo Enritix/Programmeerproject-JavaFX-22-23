3d7b4c0d226c52fbd9e915d0dae908dbd2f60cf7
Enritix <crazykillergames2@hotmail.com>
2023-02-15 14:47:50 +0100

    main files

---
+++ BoterKaasEieren/src/be/kdg/aaron/enrico/boterkaaseieren/domain/Main.java
@@ -0,0 +1,14 @@
+package be.kdg.aaron.enrico.boterkaaseieren.domain;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
