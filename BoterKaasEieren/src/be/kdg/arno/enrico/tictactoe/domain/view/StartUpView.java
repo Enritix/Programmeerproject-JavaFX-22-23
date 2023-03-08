16fed780ae35c7da6542d5818751a5e479db8be6
Enrico Egghe <crazykillergames2@hotmail.com>
2023-03-08 16:45:12 +0100

    v1.5

--- BoterKaasEieren/src/be/kdg/arno/enrico/tictactoe/domain/view/StartUpView.java
+++ BoterKaasEieren/src/be/kdg/arno/enrico/tictactoe/domain/view/StartUpView.java
@@ -13,6 +13,11 @@ import javafx.scene.text.Font;
 
 public class StartUpView extends BorderPane {
 
+    private static final int BUTTONPREFWIDTH = 440;
+    private static final int BUTTONPREFHEIGHT = 20;
+    private static final int SCOREBOARDPREFWIDTH = 300;
+    private static final int SCOREBOARDPREFHEIGHT = 600;
+
 
     Label scoreboardTitle;
     Label title;
@@ -39,9 +44,24 @@ public class StartUpView extends BorderPane {
     private void initialiseNodes() {
 
         scoreboardTitle = new Label("Scoreboard");
-        scoreboardTitle.setFont(Font.font("Verdana", 30));
-        scoreboardTitle.setStyle("-fx-text-fill: #68C8FF;");
-        scoreboardTitle.setMinWidth(0);
+        scoreboardTitle.setFont(Font.font("Verdana", 35));
+        scoreboardTitle.setStyle("-fx-text-fill: #000000;");
+
+        p1.setFont(Font.font("Verdana", 30));
+        p1.setStyle("-fx-text-fill: #68C8FF;");
+
+        p2.setFont(Font.font("Verdana", 30));
+        p2.setStyle("-fx-text-fill: #68C8FF;");
+
+        p3.setFont(Font.font("Verdana", 30));
+        p3.setStyle("-fx-text-fill: #68C8FF;");
+
+        p4.setFont(Font.font("Verdana", 30));
+        p4.setStyle("-fx-text-fill: #68C8FF;");
+
+        p5.setFont(Font.font("Verdana", 30));
+        p5.setStyle("-fx-text-fill: #68C8FF;");
+
 
 
         title = new Label("Boter, kaas & eieren");
@@ -51,22 +71,22 @@ public class StartUpView extends BorderPane {
         play1v1 = new Button("Play 1 versus 1");
         play1v1.setFont(Font.font("Verdana", 30));
         play1v1.setStyle("-fx-background-color: #032056; -fx-text-fill: #68C8FF; -fx-background-radius: 5px");
-        play1v1.setPrefSize(440,20);
+        play1v1.setPrefSize(BUTTONPREFWIDTH,BUTTONPREFHEIGHT);
 
         playComputer = new Button("Play against the computer");
         playComputer.setFont(Font.font("Verdana", 30));
         playComputer.setStyle("-fx-background-color: #032056; -fx-text-fill: #68C8FF; -fx-background-radius: 5px");
-        playComputer.setPrefSize(440,20);
+        playComputer.setPrefSize(BUTTONPREFWIDTH,BUTTONPREFHEIGHT);
 
         rules = new Button("Rules");
         rules.setFont(Font.font("Verdana", 30));
         rules.setStyle("-fx-background-color: #032056; -fx-text-fill: #68C8FF; -fx-background-radius: 5px");
-        rules.setPrefSize(440,20);
+        rules.setPrefSize(BUTTONPREFWIDTH,BUTTONPREFHEIGHT);
 
         exit = new Button("Exit the game");
         exit.setFont(Font.font("Verdana", 30));
         exit.setStyle("-fx-background-color: #032056; -fx-text-fill: #68C8FF; -fx-background-radius: 5px");
-        exit.setPrefSize(440,20);
+        exit.setPrefSize(BUTTONPREFWIDTH,BUTTONPREFHEIGHT);
 
     }
 
@@ -90,16 +110,23 @@ public class StartUpView extends BorderPane {
         this.setTop(hbox);
         hbox.getChildren().add(title);
 
+        VBox topPlayers = new VBox();
+        topPlayers.setAlignment(Pos.TOP_LEFT);
+        topPlayers.setPrefSize(SCOREBOARDPREFWIDTH,SCOREBOARDPREFHEIGHT - 30);
+        topPlayers.getChildren().addAll(p1, p2, p3, p4, p5);
+        topPlayers.setSpacing(20);
+        topPlayers.setStyle("-fx-background-color: #032056; -fx-background-radius: 5px, 50px, 5px, 50px");
+        VBox.setMargin(topPlayers, new Insets(15));
+
+
         VBox scorebox = new VBox();
         scorebox.setAlignment(Pos.CENTER);
-        scorebox.setSpacing(30);
-        scorebox.setPrefSize(300, 600);
-        scorebox.setStyle("-fx-background-color: #032056; -fx-background-radius: 5px, 50px, 5px, 50px");
-        scorebox.getChildren().addAll(scoreboardTitle, p1, p2, p3, p4, p5);
-        scorebox.setAlignment(Pos.TOP_CENTER);
+        scorebox.setPrefSize(SCOREBOARDPREFWIDTH, SCOREBOARDPREFHEIGHT);
+        scorebox.setStyle("-fx-background-color: #3902ed; -fx-background-radius: 5px, 50px, 5px, 50px");
+        scorebox.getChildren().addAll(scoreboardTitle, topPlayers);
+        scorebox.setAlignment(Pos.CENTER);
         this.setRight(scorebox);
         BorderPane.setMargin(scorebox, new Insets(20));
-
     }
 
     public Button getPlay1v1() {
